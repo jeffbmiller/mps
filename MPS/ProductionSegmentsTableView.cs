26fd44f9fa4577a5a724cbb3409e7fa04ea5b9db
jeffbmiller <jeffmiller11@hotmail.com>
2014-02-25 20:31:05 -0600

    Work to Custom Cells

--- MPS/ProductionSegmentsTableView.cs
+++ MPS/ProductionSegmentsTableView.cs
@@ -10,7 +10,7 @@ namespace MPS
     {
         public ProductionSegmentsTableView(System.Drawing.RectangleF frame):base(frame, UITableViewStyle.Grouped)
         {
-            Source = new MyTableViewSource(new List<string>(){"Jeff","Cool"});
+            Source = new MyTableViewSource(new List<string>(){"12345 Product A - Chilled","54321 Product B - Frozen"});
 
         }
     }
@@ -45,13 +45,14 @@ namespace MPS
 
         public ProductionSegmentCell(string cellId):base(UITableViewCellStyle.Default, cellId)
         {
-//            ContentView.BackgroundColor = UIColor.Blue;
+            ContentView.BackgroundColor = UIColor.Blue;
             titleLabel = new UILabel()
             {
                 Font = UIFont.FromName("Helvetica-Bold", 12f),
-                TextColor = UIColor.Blue,
+                TextColor = UIColor.White,
+				BackgroundColor = UIColor.Clear
             };
-            ContentView.Add(TextLabel);
+            ContentView.Add(titleLabel);
         }
 
         public void UpdateCell(string title)
@@ -62,7 +63,7 @@ namespace MPS
         public override void LayoutSubviews()
         {
             base.LayoutSubviews();
-            titleLabel.Frame = Frame = new RectangleF(5, 1, ContentView.Bounds.Width-63, 25);
+            titleLabel.Frame  = new RectangleF(5, 1, ContentView.Bounds.Width, 25);
         }
     }
 }
